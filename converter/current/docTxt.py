ff5c2b04825954b6135bf4527ba77b493a5b031f
Siegfried Kienzle <siegfried.kienzle@gmx.de>
2016-10-22 11:51:20 +0200

    remove bugs on docTxt.py

--- converter/current/docTxt.py
+++ converter/current/docTxt.py
@@ -1,13 +1,14 @@
 """ Modul zur Umwandlung von einer .doc zu einer .txt """
 
 import subprocess 
-
+import logging
 
 def doc_txt(filename):
+    stderr = None 
     try:
-        process = subprocess.Popen(['catdoc','-w', filename], stdout=subprocess.PIPE)
+        process = subprocess.Popen(['catdoc','-w', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         process.wait()
         return process.stdout.read()
-    
     except:
-        logging.error("filed to process " + filename)
+        logging.error(stderr)
+	#logging.error("filed to process " + filename)
