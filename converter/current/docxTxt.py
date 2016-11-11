e7e0d3c9f3ac73e0014d611dd33f510bd7f256fd
Siegfried Kienzle <sikienzl@htwg-konstanz.de>
2016-11-11 09:16:15 +0100

    add exception-handling

--- converter/current/docxTxt.py
+++ converter/current/docxTxt.py
@@ -5,9 +5,11 @@ import logging
 
 
 def docx_txt(filename):
-    stderr = None
+    text = ""
     try:
         text = docx2txt.process(filename)
         return text.encode()
-    except:
-        logging.error(stderr)
+    except AttributeError as aErr:
+        logging.error(aErr)
+    except FileNotFoundError as fErr:
+        logging.error(fErr)
