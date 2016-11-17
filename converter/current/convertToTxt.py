1d8a3f84fb4fd87bd08a95eba6950d63a08f5a31
Siegfried Kienzle <sikienzl@htwg-konstanz.de>
2016-11-17 17:27:19 +0100

    move method process and file into the file extractTxt.py

--- converter/current/convertToTxt.py
+++ converter/current/convertToTxt.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """Converts any files into txt-files"""
 
-import docTxt
-import docxTxt
-import pdfTxt
-import rtfTxt
-import odtTxt
 import sys
 import getopt
 import logging
 import os.path
+import extractTxt
 
 logFileName = 'logfile.log'
 
@@ -45,52 +41,21 @@ def main():
         elif o in ("-h", "--help"):  # help
             print(help())
         elif o in ("-p", "--process"):  # help
-            text = process(a)
+            text = extractTxt.process(a)
         elif o in ("-o", "--output"):
-            file(text, a)
+            extractTxt.file(text, a)
         else:
             logging.info('False argument')
             print("Please put a correct parameter: error \n" + help())
     if verbose == True:
        print(text)
 
-def process(path):
-    if(os.path.isfile(path)):
-        logging.info('Started text extract')
-        filename, extension = path.split(".")
-        if extension == "doc":
-            text = docTxt.doc_txt(path)
-        if extension == "docx":
-            text = docxTxt.docx_txt(path)
-        if extension == "pdf":
-            text = pdfTxt.pdf_txt(path)
-        if extension == "rtf":
-            text = rtfTxt.rtf_txt(path)
-        if extension == "odt":
-            text = odtTxt.odt_txt(path)
-        logging.info('End text extract')
-        encodedText = text.decode('utf-8')
-        return encodedText
-    else:
-        logging.error('File not exist')
-        print("File not exist: error \n" + help())
-        sys.exit(2)
-
-def file(text, a):
-    if text != None or a != None:
-        text_file=open(a,"w+")
-        text_file.write(text)
-        text_file.close()
-    else:
-        logging.error('Correct use: convertToTxt.py -p <PATH> -o <OUTPUTPATH>')
-        print("Correct use: convertToTxt.py -p <PATH> -o <OUTPUTPATH>")
-
 def help():
     return("arguments:\n" +
-           "-h, --help                                      show help message and exit\n" +
-           "-p [path to file]  --process [path to file]     to run the program")
-
-
+           "-h, --help                                               show help message and exit\n" +
+           "-p [path to file]        --process [path to file]        to run the program\n" +
+           "-o [path to output-file] --output  [path to output-file] to extract text into file (works only with the argument -p)\n" +
+           "-v                                                       verbose-Mode")
 
 if __name__ == '__main__':
     main()
---
+++ converter/current/extractTxt.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Modul with logic"""
+
+import docTxt
+import docxTxt
+import pdfTxt
+import rtfTxt
+import odtTxt
+import sys
+import getopt
+import logging
+import os.path
+
+def process(path):
+    if(os.path.isfile(path)):
+        logging.info('Started text extract')
+        filename, extension = path.split(".")
+        if extension == "doc":
+            text = docTxt.doc_txt(path)
+        if extension == "docx":
+            text = docxTxt.docx_txt(path)
+        if extension == "pdf":
+            text = pdfTxt.pdf_txt(path)
+        if extension == "rtf":
+            text = rtfTxt.rtf_txt(path)
+        if extension == "odt":
+            text = odtTxt.odt_txt(path)
+        logging.info('End text extract')
+        encodedText = text.decode('utf-8')
+        return encodedText
+    else:
+        logging.error('File not exist')
+        print("File not exist: error \n" + help())
+        sys.exit(2)
+
+def file(text, a):
+    if text != None or a != None:
+        text_file=open(a,"w+")
+        text_file.write(text)
+        text_file.close()
+    else:
+        logging.error('Correct use: convertToTxt.py -p <PATH> -o <OUTPUTPATH>')
+        print("Correct use: convertToTxt.py -p <PATH> -o <OUTPUTPATH>")
