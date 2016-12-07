7e0ae86ea5e08a4cd9dcbdc2327f47f31538c1d2
Mark Unger <mark.unger@htwg-konstanz.de>
2016-12-07 14:26:36 -0300

    added module for text recognizing

---
+++ seperator/texterkennung.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Python
+def LD(s,t):
+    s = ' ' + s
+    t = ' ' + t
+    d = {}
+    S = len(s)
+    T = len(t)
+    for i in range(S):
+        d[i, 0] = i
+    for j in range (T):
+        d[0, j] = j
+    for j in range(1,T):
+        for i in range(1,S):
+            if s[i] == t[j]:
+                d[i, j] = d[i-1, j-1]
+            else:
+                d[i, j] = min(d[i-1, j], d[i, j-1], d[i-1, j-1]) + 1
+    return d[S-1, T-1]
+    
+    
+stringa = "Dies ist Seite 1"
+stringb = "Dies ist Seite 100"
+
+
+print(LD(stringa,stringb))
