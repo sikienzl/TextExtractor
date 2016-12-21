de419fdfab24101a0476cd0ad9f599695ef25ef7
Mark Unger <marunger@htwg-konstanz.de>
2016-12-21 18:13:33 +0100

    improved checker2

--- seperator/checker2.py
+++ seperator/checker2.py
@@ -2,52 +2,37 @@
 
 import difflib
 
-MAXLINES = 55
-
 def checking():
-        file1 = open("test.txt", "r")
-        file2 = open("tmp_test.txt", "r")
+        input_file = open("test.txt", "r")
+        output_file = open('example3.txt', 'wt')
         liste = []
-        tmp_liste = []
+        dictionary = {}
         listeFinal = []
         unterschied = 0
         count = 0
-        for line1 in file1:
-                str1 = line1.rstrip()
-                liste.append(str1)
-        tmp_liste = liste
+        one_line = ''
+        ueberspringen=0
+        for line in input_file:
+                one_line = line.rstrip()
+                if(one_line != ''):
+                    for i in reversed(liste):
+                        unterschied = difflib.SequenceMatcher(None, i,one_line)
+                        prozent=unterschied.ratio()*100
+                        if(prozent > 80):
+                            ueberspringen=1
+                            continue
+                        count += 1
+                        if(count > 100):
+                            count = 0
+                            break
+                    if(ueberspringen == 0):
+                        liste.append(one_line)
+                    else:
+                        ueberspringen = 0
+
         for i in liste:
-            #print(i)
-            for j in tmp_liste:
-                unterschied = LD(i, j)
-                if unterschied == 65 and count < len(tmp_liste):
-                #if unterschied > 64 and count < len(tmp_liste):
-                   print(unterschied)
-                   print(i) 
-                   listeFinal.append(j) 
-                   count = count + 1
-                   continue
-
-        file1.close()
-        print(listeFinal) 
-
-
-def LD(s,t):
-    s = ' ' + s
-    t = ' ' + t
-    d = {}
-    S = len(s)
-    T = len(t)
-    for i in range(S):
-        d[i, 0] = i
-    for j in range (T):
-        d[0, j] = j
-    for j in range(1,T):
-        for i in range(1,S):
-            if s[i] == t[j]:
-                d[i, j] = d[i-1, j-1]
-            else:
-                d[i, j] = min(d[i-1, j], d[i, j-1], d[i-1, j-1]) + 1
-    return d[S-1, T-1]
+            output_file.write(i+'\n')
+        input_file.close()
+        output_file.close()
 
 checking()
