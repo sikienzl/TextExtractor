6648fd1d9cfde0fb89aeb6ddbeccfc85e412ac14
Mark Unger <marunger@htwg-konstanz.de>
2016-12-23 00:26:28 +0100

    refactoring

--- seperator/checker2.py
+++ seperator/checker2.py
@@ -7,43 +7,14 @@ MAX_LINE_COUNT = 200
 INPUT_FILE = open("test.txt", "r")
 OUTPUT_FILE = open('example3.txt', 'wt')
 
-def checking():
-        liste_wiederholungen = []
-        liste_zwischen = []
-        count_empty_list = 0
-        count_liste_zwischen = 0
-        list_without_empty_lines = delete_empty_lines(INPUT_FILE)
-        for one_line in list_without_empty_lines:
-            count_empty_list += 1
-            for a in reversed(liste_zwischen):
-                count_liste_zwischen += 1
-                unterschied = difflib.SequenceMatcher(None, one_line,a)
-                prozent=unterschied.ratio()*100
-                if(prozent > MAX_PROCENT):
-                    is_repetition = False
-                    for i in liste_wiederholungen:
-                        unterschied = difflib.SequenceMatcher(None, i,a)
-                        prozent=unterschied.ratio()*100
-                        if(prozent > MAX_PROCENT):
-                            is_repetition = True
-                            break
-                    if(is_repetition == False):
-                        liste_wiederholungen.append(a)
-                if(count_liste_zwischen > MAX_LINE_COUNT):
-                    count_liste_zwischen = 0
-                    break
-            liste_zwischen.append(one_line)
-            if(count_empty_list > MAX_LINE_COUNT):
-                break
-        output_liste = extract_repeted_lines(list_without_empty_lines, liste_wiederholungen)
-        for i in output_liste:
-            OUTPUT_FILE.write(i+'\n')
-        INPUT_FILE.close()
-        OUTPUT_FILE.close()
-
-
-
-
+def main():
+    list_without_empty_lines = delete_empty_lines(INPUT_FILE)
+    liste_wiederholungen = get_wiederholung_list(list_without_empty_lines)
+    output_liste = extract_repeted_lines(list_without_empty_lines, liste_wiederholungen)
+    for i in output_liste:
+        OUTPUT_FILE.write(i+'\n')
+    INPUT_FILE.close()
+    OUTPUT_FILE.close()
 
 def extract_repeted_lines(list_without_empty_lines, liste_wiederholungen):
     liste_final = []
@@ -67,6 +38,34 @@ def delete_empty_lines(input_file):
                 list_without_empty_lines.append(one_line)
     return list_without_empty_lines
 
+def get_wiederholung_list(list_without_empty_lines):
+    liste_zwischen = []
+    liste_wiederholungen = []
+    count_liste_zwischen = 0
+    count_empty_list = 0
+    for one_line in list_without_empty_lines:
+        count_empty_list += 1
+        for a in reversed(liste_zwischen):
+            count_liste_zwischen += 1
+            unterschied = difflib.SequenceMatcher(None, one_line,a)
+            prozent=unterschied.ratio()*100
+            if(prozent > MAX_PROCENT):
+                is_repetition = False
+                for i in liste_wiederholungen:
+                    unterschied = difflib.SequenceMatcher(None, i,a)
+                    prozent=unterschied.ratio()*100
+                    if(prozent > MAX_PROCENT):
+                        is_repetition = True
+                        break
+                if(is_repetition == False):
+                    liste_wiederholungen.append(a)
+            if(count_liste_zwischen > MAX_LINE_COUNT):
+                count_liste_zwischen = 0
+                break
+        liste_zwischen.append(one_line)
+        if(count_empty_list > MAX_LINE_COUNT):
+            break
+    return liste_wiederholungen
 
-
-checking()
+if __name__ == "__main__":
+    main()
