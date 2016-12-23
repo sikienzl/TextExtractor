f320eceb6cefd152451ffd523dacff0a90b758c9
Siegfried Kienzle <sikienzl@htwg-konstanz.de>
2016-12-23 15:19:07 +0100

    add a help and options

--- seperator/trainy.py
+++ seperator/trainy.py
@@ -3,9 +3,56 @@
 import nltk.tokenize.punkt
 import pickle
 import codecs
+import sys
+import getopt
+
 tokenizer = nltk.tokenize.punkt.PunktSentenceTokenizer()
-text = codecs.open("master_bp.txt", "r", "utf8").read()
-tokenizer.train(text)
-out = open("business.pickle", "wb")
-pickle.dump(tokenizer, out)
-out.close()
+
+def main():
+    argv = sys.argv[1:]
+    text = None
+    if(len(sys.argv) == 1):
+       print("Please put a correct parameter!\n")
+       print(help())
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ['help', 'input=', 'output='])
+    except getopt.GetoptError as e:
+        print("Please put a correct parameter!\n")
+    for o, a in opts:
+        if o in ("-h", "--help"):
+           print(help())
+        elif o in ("-i", "--input"):
+           text = getText(a)
+        elif o in ("-o", "--output"):
+           if a != None:
+              writePickleFile(text, a)
+           else:
+              print("No filename for output-file\n")
+        else:
+           print(help())           
+    #tokenizer = nltk.tokenize.punkt.PunktSentenceTokenizer()
+    #text = codecs.open("master_bp.txt", "r", "utf8").read()
+    #tokenizer.train(text)
+    #out = open("business.pickle", "wb")
+    #pickle.dump(tokenizer, out)
+    #out.close()
+
+def getText(filename):
+    tokenizer = nltk.tokenize.punkt.PunktSentenceTokenizer()
+    text = codecs.open(filename, "r", "utf8").read()
+    return text
+
+def writePickleFile(text, filename):
+    tokenizer.train(text)
+    out = open(filename, "wb")
+    pickle.dump(tokenizer, out)
+    out.close()
+
+def help():
+    return("arguments\n" +
+       "-h,                        --help                              show help message and exit\n" +
+       "-i [path to file]          --input [path to file]              read the input-file\n" +
+       "-o [path to outputfile]    --output [path to outputfile]       to write data into file\n")
+
+if __name__ == "__main__":
+    main()
