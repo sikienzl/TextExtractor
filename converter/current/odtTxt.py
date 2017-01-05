cdc2bd7af8900840b782a668b5e7dd183daeb041
Siegfried Kienzle <sikienzl@htwg-konstanz.de>
2017-01-05 17:26:53 +0100

    odtTxt.py pep8 konform

--- converter/current/odtTxt.py
+++ converter/current/odtTxt.py
@@ -8,10 +8,11 @@ def odt_txt(filename):
     stderr = None
     try:
         process = subprocess.Popen(
-            ['odt2txt', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            ['odt2txt', filename],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         text = process.stdout.read()
         process.stdout.close()
         return text
     except Exception as e:
         logging.error(e)
-        #logging.error(stderr)
+        # logging.error(stderr)
