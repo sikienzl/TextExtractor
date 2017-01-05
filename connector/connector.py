8712973dd3f0e265cf27197623762b1e6ec559ab
Siegfried Kienzle <sikienzl@htwg-konstanz.de>
2017-01-05 18:43:17 +0100

    connector.py pep8 konform

--- connector/connector.py
+++ connector/connector.py
@@ -7,23 +7,23 @@ config = configparser.ConfigParser()
 config.read('connect.cfg')
 
 try:
-   #CONNECTION-Settings
-   conn = mariadb.connect(
-     host=config['CONNECTION']['host'],
-     user=config['CONNECTION']['user'],
-     passwd=config['CONNECTION']['passwd'],
-     db=config['CONNECTION']['db'],
-     charset=config['CONNECTION']['charset']
-   )
-   cursor = conn.cursor()
-   sql="INSERT INTO DVD(dvd_id, titel) VALUES (%s, %s)"
-   cursor.execute(sql,('2', 'Verrückte Weihnachten'))
-   #write values hard into database
-   conn.commit()
-   #close all
-   cursor.close()
-   conn.close()
+    # CONNECTION-Settings
+    conn = mariadb.connect(
+        host=config['CONNECTION']['host'],
+        user=config['CONNECTION']['user'],
+        passwd=config['CONNECTION']['passwd'],
+        db=config['CONNECTION']['db'],
+        charset=config['CONNECTION']['charset']
+    )
+    cursor = conn.cursor()
+    sql = "INSERT INTO DVD(dvd_id, titel) VALUES (%s, %s)"
+    cursor.execute(sql, ('2', 'Verrückte Weihnachten'))
+    # write values hard into database
+    conn.commit()
+    # close all
+    cursor.close()
+    conn.close()
 except Exception as e:
-   print('Connection Failed!\nError Code is %s;\nError Content is %s;'%(e.args[0],e.args[1]))
-
-
+    print(
+        'Connection Failed!\nError Code is %s;\nError Content is %s;' %
+        (e.args[0], e.args[1]))
