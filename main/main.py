e4cabe6d4fb1f603d54db86f36e3668a03ab854c
Siegfried Kienzle <siegfried.kienzle@gmx.de>
2017-02-20 17:24:07 +0100

    add option o

--- main/main.py
+++ main/main.py
@@ -19,7 +19,9 @@ import configparser
 config = configparser.ConfigParser()
 config.read('path.cfg')
 
+
 def main():
+    output = './'
     argv = sys.argv[1:]
     string = None
 #    opts = None
@@ -29,10 +31,13 @@ def main():
 
 #    try:
     opts, args = getopt.getopt(
-            argv, "hi:p:d", ['help', 'input=', 'path=', 'delete'])
+        argv, "hi:o:p:d", ['help', 'input=', 'output=', 'path=', 'delete'])
 #    except getopt.GetoptError as e:
 #        loggingModule.logger9.info("Please put a correct parameter!\n")
 
+    output_flag = False
+    input_flag = False
+    outputpath = ""
     for o, a in opts:
         if o in ("-h", "--help"):
             loggingModule.logger9.info(help())
@@ -41,12 +46,24 @@ def main():
             work(input_file)
         elif o in ("-p", "--path"):
             path = a
-            path_with_files(path)
+            input_flag = True
+            # path_with_files(path)
+        elif o in ("-o", "--output"):
+            outputpath = a
+            output_flag = True
         else:
             loggingModule.logger9.info(help())
 
+    if output_flag and input_flag:
+        output = outputpath
+        if not os.path.exists(output):
+            os.makedirs(output)
+        path_with_files(path, output + '/')
+    elif input_flag:
+        path_with_files(path, output)
 
-def path_with_files(path):
+
+def path_with_files(path, output):
     endings = ['pdf', 'rtf', 'doc', 'docx', 'odt']
     listr = []
     encoding = 'utf-8'
@@ -73,7 +90,7 @@ def path_with_files(path):
         input_path = path + clean_filename
         loggingModule.logger9.info(input_path + " goes in process")
 
-        work(input_path, clean_filename)
+        work(input_path, clean_filename, output)
 
     loggingModule.logger9.info("----------READY----------")
 '''
@@ -83,17 +100,17 @@ The tree files _converted, _removed_lines and _final stay where the input_file i
 '''
 
 
-def work(input_file, filename):
-    converter_path=config['PFAD']['converter'] 
-    checker_path=config['PFAD']['checker'] 
-    seperator_path=config['PFAD']['seperator'] 
+def work(input_file, filename, output):
+    converter_path = config['PFAD']['converter']
+    checker_path = config['PFAD']['checker']
+    seperator_path = config['PFAD']['seperator']
 
     file_name_process = input_file.split('.')
     file_name = file_name_process[0]
     right_filename = filename.split('.')
     file_name2 = str(right_filename[0])
 
-    file_name_converted = file_name2 + '_converted.txt'
+    file_name_converted = output + file_name2 + '_converted.txt'
     try:
         proc = subprocess.Popen(
             ["python3",
@@ -107,7 +124,7 @@ def work(input_file, filename):
     except Exception as e:
         loggingModule.logger9.error("Cannot convert file: " + str(e))
 
-    file_name_removedLines = file_name2 + '_removed_lines.txt'
+    file_name_removedLines = output + file_name2 + '_removed_lines.txt'
     try:
         proc1 = subprocess.Popen(
             ["python3",
@@ -122,7 +139,7 @@ def work(input_file, filename):
     except Exception as e:
         loggingModule.logger9.error("Cannot check file: " + str(e))
 
-    file_name_seperator = file_name2 + '_final.txt'
+    file_name_seperator = output + file_name2 + '_final.txt'
     try:
         proc2 = subprocess.Popen(
             ["/usr/bin/python3",
@@ -140,12 +157,14 @@ def work(input_file, filename):
 
 def help():
     return("arguments\n" +
-           "-h,                      --help                          " +
+           "-h,                        --help                          " +
            "show help message and exit\n" +
-           "-i [path to file]        --input  [path to file]         " +
+           "-i [path to file]          --input  [path to file]         " +
            "to run the program\n" +
-           "-p [path to folder]  --path [path to folder]   " +
-           "process all file in a folder\n")
+           "-p [path to folder]        --path [path to folder]   " +
+           "process all file in a folder\n" +
+           "-o [path to outputfolder]  --output [path to outputfolder] " +
+           "write output into a folder\n")
 
 if __name__ == '__main__':
     main()
