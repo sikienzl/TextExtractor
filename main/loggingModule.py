972a6b93d62a5922d55ec0ddff40f10f505c5075
Siegfried Kienzle <siegfried.kienzle@gmx.de>
2017-02-27 15:51:04 +0100

    remove permission-Error

--- main/loggingModule.py
+++ main/loggingModule.py
@@ -10,6 +10,8 @@ import logging.handlers
 import os
 import errno
 
+LOG_FILE = os.path.abspath(os.path.dirname(__file__)) + "/logging.log"
+
 # logger for console and file with maxbyte=1mb
 logging.getLogger().setLevel(logging.INFO)
 
@@ -23,12 +25,12 @@ console.setFormatter(formatter)
 console.setLevel(logging.INFO)
 
 # file
-filehandler = logging.FileHandler(os.path.abspath(os.path.dirname(__file__)) + "/logging.log")
+filehandler = logging.FileHandler(LOG_FILE)
 file_formatter = logging.Formatter(
     '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 filehandler = logging.handlers.RotatingFileHandler(
-    "logging.log",
-    mode='a',
+    LOG_FILE,
+    mode="wa",
     maxBytes=1000000,
     backupCount=1)
 filehandler.setFormatter(file_formatter)
