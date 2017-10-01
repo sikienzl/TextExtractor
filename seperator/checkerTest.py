79c2e063cafb3d5e252866dbe0e018de71ee682d
sikienzl <siegfried.kienzle@gmx.de>
2017-10-01 13:09:29 +0200

    add unittest

--- seperator/checkerTest.py
+++ seperator/checkerTest.py
@@ -1,3 +1,4 @@
+import unittest
 import checker
 
 delete_empty_lines1 = '''
@@ -8,10 +9,10 @@ delete_empty_lines2 = []
 get_wiederholung_list1 = ['dies ist ein test', 'dies ist ein test', 'Der Bär war gesund','dies ist ein test','dies ist ein test','dies ist ein test','dies ist ein test']
 get_wiederholung_list2 = ['dies ist ein test']
 
+class checkerTest(unittest.TestCase):
+    def test_delete_empty_lines(self):
+        self.assertEqual(checker.delete_empty_lines(delete_empty_lines1), delete_empty_lines2)
 
-def test_delete_empty_lines():
-    eq_(checker.delete_empty_lines(delete_empty_lines1), delete_empty_lines2)
 
-
-def test_get_wiederholung_list():
-    eq__(checker.get_wiederholung_list(get_wiederholung_list1), get_wiederholung_list2)
+    def test_get_wiederholung_list(self):
+        self.assertEqual(checker.get_wiederholung_list(get_wiederholung_list1), get_wiederholung_list2)
