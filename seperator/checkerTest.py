ee869113d11884313bfbc1964e473584b66de5a7
sikienzl <siegfried.kienzle@gmx.de>
2017-10-01 13:01:23 +0200

    remove self on checkerTest

--- seperator/checkerTest.py
+++ seperator/checkerTest.py
@@ -10,8 +10,9 @@ get_wiederholung_list1 = ['dies ist ein test', 'dies ist ein test', 'Der Bär wa
 get_wiederholung_list2 = ['dies ist ein test']
 
 
-def test_delete_empty_lines(self):
+def test_delete_empty_lines():
     operator.eq_(checker.delete_empty_lines(delete_empty_lines1), delete_empty_lines2)
 
-def test_get_wiederholung_list(self):
+
+def test_get_wiederholung_list():
     operator.eq__(checker.get_wiederholung_list(get_wiederholung_list1), get_wiederholung_list2)
